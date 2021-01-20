b62fe61febc1cd545c0ad8b2bf01be6a982a1746
Yuri Pinheiro <yuripinheirot@gmail.com>
2021-01-19 23:37:54 -0400

    Criado footer com sucesso!

--- src/components/ContainerHome.js
+++ src/components/ContainerHome.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import MaterialHeader from './MaterialUI/MaterialHeader'
+import MaterialFooter from './MaterialUI/MaterialFooter'
 
+const Body = styled.div`
+  display: flex;
+  height: 81.7vh ;
+`
 
 class ContainerHome extends Component {
   render() {
     return (
       <div>
-        <MaterialHeader />  
+        <MaterialHeader />
+        <Body>
+
+        </Body>
+        <MaterialFooter />
       </div>
     )
   }
