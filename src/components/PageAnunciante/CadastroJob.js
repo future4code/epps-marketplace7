7e7641700378a54cc2763fb24cd958abc5959bcf
franciane brito <francianebrito99@gmail.com>
2021-01-20 15:08:12 -0300

    Layout pagina anunciante

--- src/components/PageAnunciante/CadastroJob.js
+++ src/components/PageAnunciante/CadastroJob.js
@@ -1,74 +1,80 @@
 import React from 'react'
 import styled from 'styled-components'
-import InstagramIcon from '@material-ui/icons/Instagram';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import WhatsAppIcon from '@material-ui/icons/WhatsApp';
-
+import Button from '@material-ui/core/Button';
 
 const ContainerFormulario = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: space-between;
 width: 65vw;
-background-color: #333;
+background-color: #8661B6;
 height: 75vh;
 align-items: center;
-color: #EEE;
-border-radius: 10px;
+border-radius: 5px;
 padding: 15px;
+border: 1px solid #333;
 
 `
 const Input = styled.input`
 width: 45vw;
 height: 3vh;
-border-radius: 4px;
-border: none;
+border-radius: 3px;
+background-color: #F5F3FC;
 padding: 5px;
+border: none;
 `
 
 const Texto = styled.textarea`
 width: 45vw;
 height: 13vh;
-border-radius: 4px;
+border-radius: 3px;
 border: none;
 padding: 5px;
 `
 const Label = styled.label`
 width: 45vw;
 border: none;
+color: #F5F3FC;
+
 `
 const Titulo = styled.h2`
 padding: 0;
+color: #333;
 `
+const StyledButton = styled(Button)`
+background-color: red;
+`
+
+
 
 export default class CadastroJob extends React.Component {
     render() {
         return (
             <ContainerFormulario>
                 <Titulo>Cadastre um novo Trabalho</Titulo>
-                    <Label>Titulo</Label>
-                    <Input />
-                    <Label>Descrição</Label>
-                    <Texto rows="4" cols="50"/>
-                    <Label>Valor da remuneração</Label>
-                    <Input />
-                    <Label>Método(s) de pagamento oferecidos</Label>
-                    <Input />
-                    <Label>Prazo</Label>
-                    <Input />
-                    <button>Cadastrar Trabalho</button>
+                <Label>Titulo</Label>
+                <Input />
+                <Label>Descrição</Label>
+                <Texto rows="4" cols="50" />
+                <Label>Valor da remuneração</Label>
+                <Input />
+                <Label>Método(s) de pagamento oferecidos</Label>
+                <Input />
+                <Label>Prazo</Label>
+                <Input />
+                <StyledButton variant="contained" color='red'  disableElevation>
+                    Cadastrar Trabalho
+                </StyledButton>
             </ContainerFormulario>
         )
     }
 }
 
 
-/* - Título
-- Descrição
-- Valor da remuneração
-- Método(s) de pagamento oferecidos
-- Prazo
-Botão */
+/* #8661B6
+#F5F3FC
+#494949 */
+
 
 
 
@@ -91,14 +97,3 @@ export default class App extends React.Component {
 }
  */
 
-/*
-logo
-
-instagram: import InstagramIcon from '@material-ui/icons/Instagram';
-
-twiter: import TwitterIcon from '@material-ui/icons/Twitter';
-
-whatsapp: import WhatsAppIcon from '@material-ui/icons/WhatsApp';
-
-termos de uso
-fale conosco */
--- src/components/PageAnunciante/Menu.js
+++ src/components/PageAnunciante/Menu.js
@@ -1,158 +1,52 @@
 import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import Styled from 'styled-components'
 import CadastroJob from './CadastroJob'
+import CardTravelIcon from '@material-ui/icons/CardTravel';
+import CreateIcon from '@material-ui/icons/Create';
 
-const drawerWidth = 400;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-   /*    duration: theme.transitions.duration.enteringScreen, */
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-   /*    duration: theme.transitions.duration.enteringScreen, */
-    }),
-  },
-  drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
-  },
- /*  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  }, */
-/*   content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  }, */
-}));
-
-export default function MiniDrawer() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  return (
-	  <div>
-    <div className={classes.root}>
-      <CssBaseline />
-      <div
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
-       
-        </Toolbar>
-      </div>
-      <Drawer
-        variant="permanent"
-        /* className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })} */
-        classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose} >
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {['Cadastrar Produtos', 'Produtos Cadastrados'].map((text, index) => (
-            <ListItem button key={text} >
-              <ListItemIcon >{index % 2 === 0 ? <InboxIcon onClick={() => {console.log("Ola mundo")}}/> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-      </Drawer>
-      
-    </div>
-	<CadastroJob />
-	  </div>
-  );
+const MenuLateral = Styled.div`
+display: flex;
+flex-direction: column;
+width: 22vw;
+border: solid 1px #8661B6;
+`
+const ContainerPage = Styled.div`
+display: flex;
+margin: 10px;
+width: 100vw;
+`
+const ContainerFormulario = Styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+margin: 10px;
+width: 99vw;
+`
+const Opcoes = Styled.h5`
+display: flex;
+color: #333;
+align-items: center;
+margin: 10px;
+&: hover {
+  color: #8661B6;
 }
+`
+export default function MiniDrawer() {
+ return (
+   <ContainerPage>
+     
+     <MenuLateral>
+     
+       <Opcoes><CardTravelIcon/> Trabalhos Cadastrados</Opcoes>
+       <Opcoes> <CreateIcon />Cadastrar novos trabalhos</Opcoes>
+     </MenuLateral>
+     <ContainerFormulario>
 
+     <CadastroJob />
+     </ContainerFormulario>
+
+   </ContainerPage>
+
+ ) 
+}
 
