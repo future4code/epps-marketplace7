d7ac9c65e049a70ef2650bd11eb071fb9375354b
fsantos22 <22fsantos@gmail.com>
2021-01-21 14:41:54 -0300

    Estrutura da página do cliente finalizada

--- src/components/PageAnunciante/ClientsPage.js
+++ src/components/PageAnunciante/ClientsPage.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
+import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
+// import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
+// import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import CardTravelIcon from "@material-ui/icons/CardTravel";
 import CreateIcon from "@material-ui/icons/Create";
 import logo from "../images/logo2.png";
-import { LogoHeader, MenuItem } from "../../Styled";
-import styled from 'styled-components';
-import { JobsList } from './../JobsList';
+import styled from "styled-components";
+import { JobsList } from "./../JobsList";
 import ContainedButtons from "../MaterialUI/MaterialButton";
 
 const drawerWidth = 240;
@@ -20,18 +27,28 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
   },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
   drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
   },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      zIndex: 1201,
+      justifyContent: `space-betwwen`,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
   drawerPaper: {
     width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: "auto",
   },
   content: {
     flexGrow: 1,
@@ -39,49 +56,113 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const NinjaDrawer = styled(Drawer)`
-.MuiDrawer-paper{
-    top: 40px;
-}
+const TopBar = styled(Toolbar)`
+  justify-content: space-between;
 `;
 
-export const ClientsPage = (props) => {
+const LogoTopBar = styled.img`
+  height: 50px;
+`;
+
+function ClientsPage(props) {
+  const { window } = props;
   const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <CardTravelIcon />
+          </ListItemIcon>
+          <ListItemText primary="Buscar Jobs" />
+        </ListItem>
+        {/* <Divider /> */}
+        <ListItem button>
+          <ListItemIcon>
+            <CreateIcon />
+          </ListItemIcon>
+          <ListItemText primary="Jobs Contratados" />
+        </ListItem>
+      </List>
+      {/* <Divider /> */}
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography className={classes.title}>
-            <LogoHeader src={logo} />
-          </Typography>
-          <ContainedButtons client={"sair"} onClick={props.onClickSair} />
-        </Toolbar>
+        <TopBar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <LogoTopBar src={logo} />
+          <ContainedButtons client={"sair"} btnClient={props.onClickSair} />
+        </TopBar>
       </AppBar>
-      <NinjaDrawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <Toolbar />
-        <div className={classes.drawerContainer}>
-          <MenuItem>
-            <CardTravelIcon /> Trabalhos Cadastrados
-          </MenuItem>
-          <Divider />
-          <MenuItem>
-            <CreateIcon />
-            Cadastrar novos trabalhos
-          </MenuItem>
-        </div>
-      </NinjaDrawer>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
       <main className={classes.content}>
-        <Toolbar />
+        <div className={classes.toolbar} />
         <JobsList />
       </main>
     </div>
   );
 }
+
+ClientsPage.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default ClientsPage;
