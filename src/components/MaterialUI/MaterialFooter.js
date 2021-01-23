c3ea9847dbd54ee780337ed43f78101b263868f5
fsantos22 <22fsantos@gmail.com>
2021-01-23 19:03:42 -0300

    responsividade do header e footer

--- src/components/MaterialUI/MaterialFooter.js
+++ src/components/MaterialUI/MaterialFooter.js
@@ -1,69 +1,107 @@
-import React from 'react'
-import styled from 'styled-components'
-import WhatsAppIcon from '@material-ui/icons/WhatsApp';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import InstagramIcon from '@material-ui/icons/Instagram'
-import FacebookIcon from '@material-ui/icons/Facebook';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import logoImage from '../images/logoH.png'
+import React from "react";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import styled from "styled-components";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import logoImage from "../images/logoH.png";
 
 const DivFooter = styled(BottomNavigation)`
-display: flex;
-background-color: #8661b6;
-justify-content: space-between;
-`
+  display: flex;
+  background-color: #8661b6;
+  justify-content: space-between;
+  flex-wrap:wrap;
+`;
 const DivButtonSocial = styled.div`
-display: flex;
-`
+  display: flex;
+
+  @media (max-width: 454px) {
+    margin: auto;
+  }
+`;
+
 const DivItemsRight = styled.div`
-display: flex;
-align-items: center;
-`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 694px) {
+    display: none;
+  }
+`;
 
 const RightButtons = styled(BottomNavigationAction)`
-color: #ffffff;
-width: 120px;
-height: 100%;
-font-size: 100px;
-`
+  color: #ffffff;
+  width: 120px;
+  /* height: 100%; */
+  font-size: 100px;
+`;
 
 const Button = styled(BottomNavigationAction)`
-padding: 0px 0px 0px 0px;
-`
+  padding: 0;
+`;
 
 const DivLogo = styled.div`
-display: flex;
-width: 120px;
-margin: 6px;
-padding: 2px 0px;
-`
+  display: flex;
+  width: 120px;
+  margin: 6px;
+  padding: 2px 0px;
+
+  @media(max-width:454px) {
+      display: none;
+  }
+`;
 
 const Logo = styled.img`
-width: 100%;
-height: 100%;
-object-fit: fill;
-`
+  width: 100%;
+`;
 
+export default function SimpleBottomNavigation() {
 
-export default class MaterialFooter extends React.Component {
-    render() {
-        return (
-            <DivFooter >
-                <DivLogo>
-                    <Logo src={logoImage} alt="Logo" />
-                </DivLogo>
-                <DivButtonSocial>
-                    <Button label="Whatsapp" value="Whatsapp" icon={<WhatsAppIcon style={{ color: '#ffffff' }} />} />
-                    <Button label="Twitter" value="Twitter" icon={<TwitterIcon style={{ color: '#ffffff' }} />} />
-                    <Button label="Instagram" value="Instagram" icon={<InstagramIcon style={{ color: '#ffffff' }} />} />
-                    <Button label="Facebook" value="Facebook" icon={<FacebookIcon style={{ color: '#ffffff' }} />} />
-                </DivButtonSocial>
-                <DivItemsRight>
-                    <RightButtons label="Fale conosco" value="Fale conosco" showLabel={true} />
-                    <RightButtons label="Termos de uso" value="Termos de uso" showLabel={true} />
-                </DivItemsRight>
-            </DivFooter>
-        )
-    }
+  return (
+    <DivFooter>
+      <DivLogo>
+        <Logo src={logoImage} alt="Logo" />
+      </DivLogo>
+      <DivButtonSocial>
+        <Button
+          label="Whatsapp"
+          value="Whatsapp"
+          icon={<WhatsAppIcon style={{ color: "#ffffff" }} />}
+        />
+
+        <Button
+          label="Twitter"
+          value="Twitter"
+          icon={<TwitterIcon style={{ color: "#ffffff" }} />}
+        />
+
+        <Button
+          label="Instagram"
+          value="Instagram"
+          icon={<InstagramIcon style={{ color: "#ffffff" }} />}
+        />
+
+        <Button
+          label="Facebook"
+          value="Facebook"
+          icon={<FacebookIcon style={{ color: "#ffffff" }} />}
+        />
+      </DivButtonSocial>
+      <DivItemsRight>
+        <RightButtons
+          label="Fale conosco"
+          value="Fale conosco"
+          showLabel={true}
+        />
+
+        <RightButtons
+          label="Termos de uso"
+          value="Termos de uso"
+          showLabel={true}
+        />
+      </DivItemsRight>
+    </DivFooter>
+  );
 }
--- src/components/MaterialUI/MaterialHeader.js
+++ src/components/MaterialUI/MaterialHeader.js
@@ -1,83 +1,68 @@
-import React from 'react';
+import React from "react";
 import styled from 'styled-components'
-import logo from '../images/logoH.png'
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import ContainedButtons from './MaterialButton';
-import { LogoHeader } from '../Styled';
-import { Typography } from '@material-ui/core';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Slide from "@material-ui/core/Slide";
+import logo from "../images/logoH.png";
+import Button from "../MaterialUI/MaterialButton"
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-const StyledBtnContainer = styled.div`
-  visibility: visible;
-  display: flex;
-
-  @media (max-width: 700px){
-    visibility: hidden;
-  }
-`
-
-const MobileHeader = styled(Toolbar)`
-  display: none;
-  
-
-  @media (max-width: 699px){
-    display: flex;
-    justify-content: center;
-  }
-
-`
-
-export default function ButtonAppBar(props) {
-  const classes = useStyles();
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography className={classes.title}>
-            <LogoHeader src={logo} onClick={props.onClickHome} />
-          </Typography>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
 
-          <StyledBtnContainer>
-            <ContainedButtons
-              client={"Quem Somos"}
-              btnClient={props.onclickAboutUs}
-            />
-            <ContainedButtons
-              client={"Cliente"}
-              btnClient={props.onClickClient}
-            />
-            <ContainedButtons
-              client={"Anunciante"}
-              btnClient={props.onClickSeller}
-            />
-          </StyledBtnContainer>
-        </Toolbar>
+const Logo = styled.img`
+height: 50px;
+`
+const ToolBar = styled(Toolbar)`
+display: flex;
+justify-content: space-between;
+flex-wrap: wrap;
 
-        <MobileHeader>
-          <ContainedButtons
-            client={"Cliente"}
-            btnClient={props.onClickClient}
-          />
-          <ContainedButtons
-            client={"Anunciante"}
-            btnClient={props.onClickSeller}
-          />
-        </MobileHeader>
-      </AppBar>
-    </div>
+.btn-container{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+@media(max-width:550px){
+  justify-content: center;
+}
+`
+
+const WrapperAppBar = styled(AppBar)`
+width: 100vw;
+left: 0;
+`
+
+export default function HideAppBar(props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <WrapperAppBar>
+          <ToolBar>
+            <Logo src={logo} alt="" />
+            <div className="btn-container">
+              <Button client="Quem somos" onClick={props.aboutUs} />
+              <Button client="Cliente" onClick={props.client} />
+              <Button id="quemSomos" client="Anunciante" onClick={props.advertiser} />
+            </div>
+          </ToolBar>
+        </WrapperAppBar>
+      </HideOnScroll>
+      <Toolbar />
+    </React.Fragment>
   );
 }
